f6323cdc9a0fcef83b43401b0d1b2b11069c512f
stvenyas <siringoringoelyas@gmail.com>
2026-03-11 19:37:33 +0700

    commit ketiga: tambah fungsi showInfo() di script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // Git Workflow Simulation Script
+// Modified by: Steven Elyas
 
 function showMessage() {
-    alert("Praktikum Git Workflow berjalan.");
+    alert("Halo! Ini kontribusi dari Steven Elyas dalam praktikum Git Workflow.");
 }
 
-console.log("Git Workflow Simulation loaded.");
+function showInfo() {
+    const info = "Nama: Steven Elyas | Praktikum: Git Workflow | Semester: 4";
+    document.body.innerHTML += `<p style="color:#1e3a8a; margin-top:10px;">${info}</p>`;
+}
+
+console.log("Git Workflow Simulation loaded. Contributor: Steven Elyas");
+console.log("Branch: feature/steven-elyas | Total Commits: 3");
